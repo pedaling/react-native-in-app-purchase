02e45ccb03af0b40cfcf4fd2b9a1ec205f379f4f
Injung Chung <mu29@yeoubi.net>
2019-07-20 10:36:04 +0900

    Implements restore method and ensure that the connection is made in each purchase flow

--- android/src/main/java/net/yeoubi/iap/RNInAppPurchaseModule.java
+++ android/src/main/java/net/yeoubi/iap/RNInAppPurchaseModule.java
@@ -62,21 +62,35 @@ public class RNInAppPurchaseModule extends ReactContextBaseJavaModule implements
         constants.put("EVENT_PURCHASE_FAILURE", "EVENT:PURCHASE_FAILURE");
         constants.put("EVENT_FINALIZE_SUCCESS", "EVENT:FINALIZE_SUCCESS");
         constants.put("EVENT_FINALIZE_FAILURE", "EVENT:FINALIZE_FAILURE");
+        constants.put("EVENT_RESTORE_SUCCESS", "EVENT:RESTORE_SUCCESS");
+        constants.put("EVENT_RESTORE_FAILURE", "EVENT:RESTORE_FAILURE");
+        constants.put("EVENT_CONNECTION_FAILURE", "EVENT:CONNECTION_FAILURE");
 
         return constants;
     }
 
+    /**
+     * Initialize billing client.
+     *
+     * @param promise Promise that receives initialization result.
+     */
     @ReactMethod
     public void init(final Promise promise) {
-        client = BillingClient.newBuilder(reactContext).enablePendingPurchases().setListener(this).build();
+        client = BillingClient.newBuilder(reactContext)
+                .enablePendingPurchases()
+                .setListener(this)
+                .build();
+
         client.startConnection(new BillingClientStateListener() {
             @Override
-            public void onBillingSetupFinished(BillingResult billingResult) {
-                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+            public void onBillingSetupFinished(BillingResult result) {
+                int code = result.getResponseCode();
+
+                if (code == BillingClient.BillingResponseCode.OK) {
                     promise.resolve(true);
                 }
 
-                promise.reject("init", "Billing service setup failed with code " + billingResult.getResponseCode());
+                promise.reject("init", "Billing service setup failed with code " + code);
             }
 
             @Override
@@ -86,33 +100,39 @@ public class RNInAppPurchaseModule extends ReactContextBaseJavaModule implements
         });
     }
 
+    /**
+     * Retrieves a list of in-app and subscription items that match the IDs contained in productIds.
+     * For iOS compatibility, this method fetches in-app and subscription items at once.
+     *
+     * @param productIds The IDs of the items to retrieve.
+     */
     @ReactMethod
     public void getProductList(ReadableArray productIds) {
-        final List<String> skuList = new ArrayList<>();
+        tryConnect(() -> {
+            final List<String> skuList = new ArrayList<>();
 
-        for (int i = 0; i < productIds.size(); i++) {
-            skuList.add(productIds.getString(i));
-        }
+            for (int i = 0; i < productIds.size(); i++) {
+                skuList.add(productIds.getString(i));
+            }
 
-        final SkuDetailsParams inAppParams = SkuDetailsParams.newBuilder()
-            .setSkusList(skuList)
-            .setType(BillingClient.SkuType.INAPP).build();
+            final SkuDetailsParams inAppParams = SkuDetailsParams.newBuilder()
+                    .setSkusList(skuList)
+                    .setType(BillingClient.SkuType.INAPP).build();
 
-        final SkuDetailsParams subscribeParams = SkuDetailsParams.newBuilder()
-            .setSkusList(skuList)
-            .setType(BillingClient.SkuType.SUBS).build();
+            final SkuDetailsParams subscribeParams = SkuDetailsParams.newBuilder()
+                    .setSkusList(skuList)
+                    .setType(BillingClient.SkuType.SUBS).build();
 
-        final WritableArray items = new WritableNativeArray();
+            final WritableArray items = new WritableNativeArray();
 
-        client.querySkuDetailsAsync(inAppParams, new SkuDetailsResponseListener() {
-            @Override
-            public void onSkuDetailsResponse(BillingResult result, List<SkuDetails> skuDetailsList) {
-                if (result.getResponseCode() != BillingClient.BillingResponseCode.OK) {
-                    sendBillingError("EVENT:GET_PRODUCT_LIST_FAILURE", result);
+            // Query in-app items
+            client.querySkuDetailsAsync(inAppParams, (inAppResult, inAppDetailsList) -> {
+                if (inAppResult.getResponseCode() != BillingClient.BillingResponseCode.OK) {
+                    sendBillingError("EVENT:GET_PRODUCT_LIST_FAILURE", inAppResult);
                     return;
                 }
 
-                for (SkuDetails skuDetails : skuDetailsList) {
+                for (SkuDetails skuDetails : inAppDetailsList) {
                     WritableMap item = Arguments.createMap();
                     item.putString("productId", skuDetails.getSku());
                     item.putString("price", skuDetails.getPrice());
@@ -124,58 +144,71 @@ public class RNInAppPurchaseModule extends ReactContextBaseJavaModule implements
                     skuDetailsMap.put(skuDetails.getSku(), skuDetails);
                 }
 
-                client.querySkuDetailsAsync(subscribeParams, new SkuDetailsResponseListener() {
-                    @Override
-                    public void onSkuDetailsResponse(BillingResult result, List<SkuDetails> skuDetailsList) {
-                        if (result.getResponseCode() != BillingClient.BillingResponseCode.OK) {
-                            sendBillingError("EVENT:GET_PRODUCT_LIST_FAILURE", result);
-                            return;
-                        }
-
-                        for (SkuDetails skuDetails : skuDetailsList) {
-                            WritableMap item = Arguments.createMap();
-                            item.putString("productId", skuDetails.getSku());
-                            item.putString("price", skuDetails.getPrice());
-                            item.putString("currency", skuDetails.getPriceCurrencyCode());
-                            item.putString("title", skuDetails.getTitle());
-                            item.putString("description", skuDetails.getDescription());
-                            item.putString("iconUrl", skuDetails.getIconUrl());
-                            items.pushMap(item);
-                            skuDetailsMap.put(skuDetails.getSku(), skuDetails);
-                        }
-
-                        sendEvent("EVENT:GET_PRODUCT_LIST_SUCCESS", items);
+                // Query subscription items
+                client.querySkuDetailsAsync(subscribeParams, (subscribeResult, subscribeDetailsList) -> {
+                    if (subscribeResult.getResponseCode() != BillingClient.BillingResponseCode.OK) {
+                        sendBillingError("EVENT:GET_PRODUCT_LIST_FAILURE", subscribeResult);
+                        return;
                     }
+
+                    for (SkuDetails skuDetails : subscribeDetailsList) {
+                        WritableMap item = Arguments.createMap();
+                        item.putString("productId", skuDetails.getSku());
+                        item.putString("price", skuDetails.getPrice());
+                        item.putString("currency", skuDetails.getPriceCurrencyCode());
+                        item.putString("title", skuDetails.getTitle());
+                        item.putString("description", skuDetails.getDescription());
+                        item.putString("iconUrl", skuDetails.getIconUrl());
+                        items.pushMap(item);
+                        skuDetailsMap.put(skuDetails.getSku(), skuDetails);
+                    }
+
+                    sendEvent("EVENT:GET_PRODUCT_LIST_SUCCESS", items);
                 });
-            }
+            });
         });
     }
 
+    /**
+     * Purchase in-app or subscription item.
+     *
+     * @param productId Unique ID of item to purchase.
+     * @param oldProductId When upgrading or downgrading a subscription, the unique ID of the
+     *                     subscription item that user originally used.
+     */
     @ReactMethod
     public void purchase(String productId, String oldProductId) {
-        BillingFlowParams.Builder builder = BillingFlowParams.newBuilder();
+        tryConnect(() -> {
+            BillingFlowParams.Builder builder = BillingFlowParams.newBuilder();
 
-        if (oldProductId != null) {
-            builder.setOldSku(oldProductId);
-        }
+            if (oldProductId != null) {
+                builder.setOldSku(oldProductId);
+            }
 
-        BillingFlowParams params = builder.setSkuDetails(skuDetailsMap.get(productId)).build();
+            BillingFlowParams params = builder.setSkuDetails(skuDetailsMap.get(productId)).build();
 
-        client.launchBillingFlow(getCurrentActivity(), params);
+            client.launchBillingFlow(getCurrentActivity(), params);
+        });
     }
 
+    /**
+     * For consumable items, call the {@link BillingClient#consumeAsync} method, otherwise call the
+     * {@link BillingClient#acknowledgePurchase} method and finish the purchase flow.
+     *
+     * @param purchase Purchase object received after payment is made.
+     * @param isConsumable Whether it is consumable or not.
+     */
     @ReactMethod
     public void finalize(ReadableMap purchase, boolean isConsumable) {
-        String token = purchase.getString("purchaseToken");
+        tryConnect(() -> {
+            String token = purchase.getString("purchaseToken");
 
-        if (isConsumable) {
-            ConsumeParams params = ConsumeParams.newBuilder()
-                .setPurchaseToken(token)
-                .build();
+            if (isConsumable) {
+                ConsumeParams params = ConsumeParams.newBuilder()
+                        .setPurchaseToken(token)
+                        .build();
 
-            client.consumeAsync(params, new ConsumeResponseListener() {
-                @Override
-                public void onConsumeResponse(BillingResult result, String purchaseToken) {
+                client.consumeAsync(params, (result, purchaseToken) -> {
                     if (result.getResponseCode() != BillingClient.BillingResponseCode.OK) {
                         sendBillingError("EVENT:FINALIZE_FAILURE", result);
                         return;
@@ -185,18 +218,15 @@ public class RNInAppPurchaseModule extends ReactContextBaseJavaModule implements
                     event.putString("message", result.getDebugMessage());
 
                     sendEvent("EVENT:FINALIZE_SUCCESS", event);
-                }
-            });
-            return;
-        }
+                });
+                return;
+            }
 
-        AcknowledgePurchaseParams acknowledgePurchaseParams = AcknowledgePurchaseParams.newBuilder()
-            .setPurchaseToken(token)
-            .build();
+            AcknowledgePurchaseParams acknowledgePurchaseParams = AcknowledgePurchaseParams.newBuilder()
+                    .setPurchaseToken(token)
+                    .build();
 
-        client.acknowledgePurchase(acknowledgePurchaseParams, new AcknowledgePurchaseResponseListener() {
-            @Override
-            public void onAcknowledgePurchaseResponse(BillingResult result) {
+            client.acknowledgePurchase(acknowledgePurchaseParams, (result) -> {
                 if (result.getResponseCode() != BillingClient.BillingResponseCode.OK) {
                     sendBillingError("EVENT:FINALIZE_FAILURE", result);
                 }
@@ -205,12 +235,52 @@ public class RNInAppPurchaseModule extends ReactContextBaseJavaModule implements
                 event.putString("message", result.getDebugMessage());
 
                 sendEvent("EVENT:FINALIZE_SUCCESS", event);
-            }
+            });
         });
     }
 
+    /**
+     * Retrieves all in-app and subscription items that were purchased but not finalized. You should
+     * validate these payments on your backend server and complete the purchase flow by calling
+     * {@link #finalize} method.
+     */
     @ReactMethod
-    public void restorePurchases() {
+    public void restore() {
+        tryConnect(() -> {
+            Purchase.PurchasesResult inAppResult = client.queryPurchases(BillingClient.SkuType.INAPP);
+            Purchase.PurchasesResult subscriptionResult = client.queryPurchases(BillingClient.SkuType.SUBS);
+
+            if (inAppResult.getResponseCode() != BillingClient.BillingResponseCode.OK) {
+                sendBillingError("EVENT:RESTORE_FAILURE", inAppResult.getBillingResult());
+                return;
+            }
+
+            if (subscriptionResult.getResponseCode() != BillingClient.BillingResponseCode.OK) {
+                sendBillingError("EVENT:RESTORE_FAILURE", subscriptionResult.getBillingResult());
+                return;
+            }
+
+            inAppResult.getPurchasesList().addAll(subscriptionResult.getPurchasesList());
+
+            WritableArray items = new WritableNativeArray();
+
+            for (Purchase purchase : inAppResult.getPurchasesList()) {
+                if (purchase.isAcknowledged()) {
+                    continue;
+                }
+
+                WritableMap item = Arguments.createMap();
+                item.putString("productId", purchase.getSku());
+                item.putString("transactionId", purchase.getOrderId());
+                item.putString("transactionDate", String.valueOf(purchase.getPurchaseTime()));
+                item.putString("receipt", purchase.getOriginalJson());
+                item.putString("purchaseToken", purchase.getPurchaseToken());
+
+                items.pushMap(item);
+            }
+
+            sendEvent("EVENT:RESTORE_SUCCESS", items);
+        });
     }
 
     private void sendEvent(String eventName, Object params) {
@@ -225,6 +295,37 @@ public class RNInAppPurchaseModule extends ReactContextBaseJavaModule implements
         event.putString("message", result.getDebugMessage());
 
         sendEvent(errorName, event);
+    }
+
+    private void sendBillingError(String errorName, Integer code, String message) {
+        WritableMap event = Arguments.createMap();
+        event.putInt("code", code);
+        event.putString("message", message);
+
+        sendEvent(errorName, event);
+    }
+
+    private void tryConnect(final Runnable runnable) {
+        if (client.isReady()) {
+            runnable.run();
+            return;
+        }
+
+        client.startConnection(new BillingClientStateListener() {
+            @Override
+            public void onBillingSetupFinished(BillingResult result) {
+                if (result.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                    runnable.run();
+                }
+
+                sendBillingError("EVENT:CONNECTION_FAILURE", result);
+            }
+
+            @Override
+            public void onBillingServiceDisconnected() {
+                sendBillingError("EVENT:CONNECTION_FAILURE", -1, "Billing service disconnected");
+            }
+        });
     }
 
     @Override
