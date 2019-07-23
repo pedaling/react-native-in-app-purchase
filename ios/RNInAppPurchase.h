b52fe0736e67793a09b5ebb719451c215ed70c79
Injung Chung <mu29@yeoubi.net>
2019-07-23 12:34:30 +0900

    Implements init, dealloc method on iOS and remove podspec

--- ios/RNInAppPurchase.h
+++ ios/RNInAppPurchase.h
@@ -1,11 +1,15 @@
 
 #if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
+#import "RCTEventEmitter.h"
 #else
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 #endif
 
-@interface RNInAppPurchase : NSObject <RCTBridgeModule>
+#import <StoreKit/StoreKit.h>
+
+@interface RNInAppPurchase : NSObject <RCTBridgeModule, SKProductsRequestDelegate, SKPaymentTransactionObserver> {
+}
 
 @end
-  
--- ios/RNInAppPurchase.m
+++ ios/RNInAppPurchase.m
@@ -1,13 +1,34 @@
-
 #import "RNInAppPurchase.h"
 
+#import <StoreKit/StoreKit.h>
+
+@interface RNInAppPurchase() <SKRequestDelegate> {
+}
+
+@end
+
 @implementation RNInAppPurchase
+
+- (instancetype) init {
+    if (self = [super init]) {
+        [[SKPaymentQueue defaultQueue] addTransactionObserver: self];
+    }
+}
+
+- (void) dealloc {
+    [[SKPaymentQueue defaultQueue] removeTransactionObserver: self];
+}
 
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_get_main_queue();
 }
-RCT_EXPORT_MODULE()
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(configure: (RCTPromiseResolveBlock) resolve
+                  rejector: (RCTPromiseRejectBlock) reject) {
+    resolve(@([SKPaymentQueue canMakePayments]));
+}
 
 @end
-  
