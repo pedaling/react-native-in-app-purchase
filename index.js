b5eed5ddf5dbdcac41340b3937f75b99141c9c96
Injung Chung <mu29@yeoubi.net>
2019-07-24 14:51:20 +0900

    Add functions to receive purchase / get product list events

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-
-import { NativeModules } from 'react-native';
+import {
+  NativeModules,
+  NativeEventEmitter,
+  DeviceEventEmitter,
+  Platform,
+} from 'react-native';
 
 const { RNInAppPurchase } = NativeModules;
 
-export default RNInAppPurchase;
+const addListener = (event, listener) => Platform.select({
+  ios: new NativeEventEmitter(RNInAppPurchase).addListener(event, listener),
+  android: DeviceEventEmitter.addListener(event, listener),
+});
+
+const onProductListSuccess = e => addListener('@iap:onProductListSuccess', e);
+
+const onProductListFailure = e => addListener('@iap:onProductListFailure', e);
+
+const onPurchaseSuccess = e => addListener('@iap:onPurchaseSuccess', e);
+
+const onPurchaseFailure = e => addListener('@iap:onPurchaseFailure', e);
+
+export default {
+  configure: RNInAppPurchase.configure,
+  getProductList: RNInAppPurchase.getProductList,
+  purchase: RNInAppPurchase.purchase,
+  restore: RNInAppPurchase.restore,
+  finalize: RNInAppPurchase.finalize,
+  onProductListSuccess,
+  onProductListFailure,
+  onPurchaseSuccess,
+  onPurchaseFailure,
+}
